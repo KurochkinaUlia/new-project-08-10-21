4072aad5ff29aee17a5a8e9e348af08364644592
Yulya <kurochkina.1@yandex.ru>
2021-10-13 20:42:40 +0300

    выполнила 2-е практическое задание к уроку 15

--- project_1/js/script.js
+++ project_1/js/script.js
@@ -1,6 +1,18 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if (numberOfFilms <=10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms >10 && numberOfFilms <30) {     
+    alert('Вы классический зритель');
+}
+ else if (numberOfFilms >=30) {     
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+} 
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,14 +22,18 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i=0; i<2; i++) {
+   const a = prompt('Один из последних просмотренных фильмов?', ''),
+         b = prompt('На сколько оцените его?', '');
 
-
-personalMovieDB.movies [a] = b;
-personalMovieDB.movies [c] = d;
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies [a] = b;
+        console.log('OK');
+    } else {
+        i--;
+        console.log ('Error');
+    }  
+}
 
 console.log (personalMovieDB);
 
